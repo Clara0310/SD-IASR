523fa9bd096983897bfcdfde827e01694c277cc6
Clara <0310qian@gmail.com>
2026-01-29 15:43:01 +0800

    modify data_loader.py content

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,5 +1,3 @@
-# 載入序列資料與圖矩陣
-
 import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
@@ -13,25 +11,33 @@ class SequentialDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, index):
-        # data 格式: [history_seq, pos_item, neg_items...]
+        # data 格式來自 7_dataset_split.py
         line = self.data[index]
-        history = line[0] # 這是 [[id, time], [id, time]...]
+        history = line[0] 
         
-        # 只提取商品 ID
-        item_history = [x[0] for x in history]
+        # 修正：增加穩定性檢查
+        item_history = []
+        for x in history:
+            if isinstance(x, (list, np.ndarray, tuple)):
+                # 如果 x 是 [item_id, timestamp]，取第一個元素
+                item_history.append(x[0])
+            else:
+                # 如果 x 直接就是 item_id (int)
+                item_history.append(x)
         
         # 進行 Padding (補 0) 或截斷
         seq = np.zeros(self.max_len, dtype=np.int64)
         idx = min(len(item_history), self.max_len)
-        seq[:idx] = item_history[-idx:]
+        if idx > 0:
+            seq[:idx] = item_history[-idx:]
         
-        # 正樣本與負樣本
+        # 正樣本與負樣本 (其餘元素)
         target_items = np.array(line[1:], dtype=np.int64)
         
         return torch.LongTensor(seq), torch.LongTensor(target_items)
 
 def get_loader(dataset_path, batch_size, max_len):
-    # 載入 7_dataset_split.py 產出的 npz
+    # 載入產出的 npz 檔案
     data = np.load(dataset_path, allow_pickle=True)
     
     train_loader = DataLoader(SequentialDataset(data['train_set'], max_len), batch_size=batch_size, shuffle=True)
