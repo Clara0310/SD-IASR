284ed9c9ccbaf1432efc2868ec4ba450b0e7e186
Clara <0310qian@gmail.com>
2026-02-07 11:09:33 +0800

    微調參數

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 DATASET="Grocery_and_Gourmet_Food"
 
 # 2. 模型基礎超參數
-EMB_DIM=64 #提升嵌入維度以增強表達能力
+EMB_DIM=128 #提升嵌入維度以增強表達能力
 BERT_DIM=768
 LR=0.001 #0.005 -> 0.001 ，降低學習率以提升訓練穩定性
 BATCH_SIZE=256
@@ -23,13 +23,13 @@ NHEAD=8      # 增加注意力頭數以提升模型表達能力
 
 #lr_scheduler 相關參數
 LR_MODE="max"     # 因為指標是 HR@10，所以是越大越好
-LR_FACTOR=0.5     # 觸發時將學習率乘以 0.5 (減半)
-LR_PATIENCE=5     # 這是排程器的耐心值（例如 5 次沒進步就降速）
+LR_FACTOR=0.1     # 觸發時將學習率乘以 0.1 
+LR_PATIENCE=15     # 這是排程器的耐心值（例如 15 次沒進步就降速）
 
 # loss 權重參數
-LAMBDA_1=0.1
-LAMBDA_2=0.1
-LAMBDA_3=0.01
+LAMBDA_1=0.01
+LAMBDA_2=0.01
+LAMBDA_3=0.00001
 
 # 4. 執行訓練指令
 # 移除了 ALPHA，並加入了 --resume 續跑與 --max_seq_len 參數
